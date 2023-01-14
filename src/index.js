9c89cbfb6202dfdcdf369d528b3015974e4ab834
rayleigh420 <nhatduy0409@gmail.com>
2023-01-14 10:03:19 +0700

    Connect db

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import mongoose from 'mongoose'
+
 require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT || 8080
+
+mongoose.connect(process.env.MONGOOSE_URL, () => {
+    console.log("Connect db access!")
+})
+
+app.use(cors())
+app.use(cookieParser)
+app.use(express.json())
 
 app.listen(port, () => {
     console.log(`-------------Web is listening on port ${port}------------`)
